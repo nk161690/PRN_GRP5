9ab2109430442f0d61ada25de365b5a9ad8abd9f
nk161690 <khanhndnhe161690@fpt.edu.vn>
2022-06-22 00:55:22 +0700

    Update ShowAddEditGUI.cs

--- SE1615_Group5_assignment1/CinemaWin/CinemaWin/CinemaWin/GUI/ShowAddEditGUI.cs
+++ SE1615_Group5_assignment1/CinemaWin/CinemaWin/CinemaWin/GUI/ShowAddEditGUI.cs
@@ -40,16 +40,12 @@ namespace CinemaWin.GUI
                 List<Show> shows = context.Shows.
                     Where(s => s.RoomId == show.RoomId
                     && s.ShowDate == show.ShowDate
-                    && s.ShowId == show.ShowId).ToList<Show>();
+                    && s.ShowId != show.ShowId).ToList<Show>();
                 foreach(Show s in shows)
-                {
                     slots[(int)s.Slot - 1] = true;
-                }
                 List<int> ls = new List<int>();
                 for (int i = 0; i < slots.Length; i++)
-                {
-                    if (slots[i] == false) ls.Add(i + 1);
-                }
+                    if (slots[i] == false) ls.Add(i + 1);              
                 cboSlot.DataSource = ls;
                 cboSlot.Text = show.Slot.ToString();
             } else
@@ -61,43 +57,45 @@ namespace CinemaWin.GUI
 
         private void cboRoomId_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int rId;
-            Int32.TryParse(cboRoomId.SelectedValue.ToString(), out rId);
-            DateTime date = dtpShowDate.Value;
-            bool[] slots = new bool[9];
-            List<Show> shows = context.Shows.
-                Where(s => s.RoomId == rId
-                && s.ShowDate == date).ToList<Show>();
-            foreach (Show s in shows)
+            if (showId == 0)
             {
-                slots[(int)s.Slot - 1] = true;
+                int rId;
+                Int32.TryParse(cboRoomId.SelectedValue.ToString(), out rId);
+                DateTime date = dtpShowDate.Value;
+                bool[] slots = new bool[9];
+                List<Show> shows = context.Shows.
+                    Where(s => s.RoomId == rId
+                    && s.ShowDate == date).ToList<Show>();
+                foreach (Show s in shows)
+                    slots[(int)s.Slot - 1] = true;
+                List<int> ls = new List<int>();
+                for (int i = 0; i < slots.Length; i++)
+                    if (slots[i] == false) ls.Add(i + 1);
+                cboSlot.DataSource = ls;
             }
-            List<int> ls = new List<int>();
-            for (int i = 0; i < slots.Length; i++)
-            {
-                if (slots[i] == false) ls.Add(i + 1);
-            }
-            cboSlot.DataSource = ls;
         }
 
         private void dtpShowDate_ValueChanged(object sender, EventArgs e)
         {
-            int rId = Convert.ToInt32(cboRoomId.SelectedValue.ToString().Trim());
-            DateTime date = dtpShowDate.Value;
-            bool[] slots = new bool[9];
-            List<Show> shows = context.Shows.
-                Where(s => s.RoomId == rId
-                && s.ShowDate == date).ToList<Show>();
-            foreach (Show s in shows)
+            if (showId == 0)
             {
-                slots[(int)s.Slot - 1] = true;
+                int rId = Convert.ToInt32(cboRoomId.SelectedValue.ToString().Trim());
+                DateTime date = dtpShowDate.Value;
+                bool[] slots = new bool[9];
+                List<Show> shows = context.Shows.
+                    Where(s => s.RoomId == rId
+                    && s.ShowDate == date).ToList<Show>();
+                foreach (Show s in shows)
+                {
+                    slots[(int)s.Slot - 1] = true;
+                }
+                List<int> ls = new List<int>();
+                for (int i = 0; i < slots.Length; i++)
+                {
+                    if (slots[i] == false) ls.Add(i + 1);
+                }
+                cboSlot.DataSource = ls;
             }
-            List<int> ls = new List<int>();
-            for (int i = 0; i < slots.Length; i++)
-            {
-                if (slots[i] == false) ls.Add(i + 1);
-            }
-            cboSlot.DataSource = ls;
         }
 
 
