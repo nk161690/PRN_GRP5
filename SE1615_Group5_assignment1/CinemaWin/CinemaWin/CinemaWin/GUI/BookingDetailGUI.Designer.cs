d2f6f2e0072470adf445f8e944fa10b0f7b85e48
trantrieu1511 <trantrieu15112k2@gmail.com>
2022-06-22 18:28:19 +0700

    design BookingDetailGUI
    GUI cua booking detail day nhe

---
+++ SE1615_Group5_assignment1/CinemaWin/CinemaWin/CinemaWin/GUI/BookingDetailGUI.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace CinemaWin.GUI
+{
+    partial class BookingDetailGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.pnlBookingDetail = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(257, 349);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(190, 27);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(257, 405);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(190, 27);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(177, 349);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(52, 20);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(164, 405);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(65, 20);
+            this.lblAmount.TabIndex = 3;
+            this.lblAmount.Text = "Amount:";
+            // 
+            // pnlBookingDetail
+            // 
+            this.pnlBookingDetail.Location = new System.Drawing.Point(237, 32);
+            this.pnlBookingDetail.Name = "pnlBookingDetail";
+            this.pnlBookingDetail.Size = new System.Drawing.Size(280, 280);
+            this.pnlBookingDetail.TabIndex = 4;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(297, 470);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(94, 29);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // BookingDetailGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 526);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.pnlBookingDetail);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.txtName);
+            this.Name = "BookingDetailGUI";
+            this.Text = "BookingDetailGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Panel pnlBookingDetail;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
